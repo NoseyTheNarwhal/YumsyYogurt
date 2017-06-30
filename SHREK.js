0ee5f4e322615d1cff9ab428c9fed98bf7198bb8
Mitchell  Walsh <noseythenarwhal@gmail.com>
2017-06-30 18:42:19 +0000

    Keyboard waffles

--- SHREK.js
+++ SHREK.js
@@ -1,3 +1,4 @@
 <script type="text/javascript">
     document.getElementById("myButton").onclick = function () {
         location.href = "http://www.thekeyboardwaffleiron.com/";
+        <embed loop="true" src="file:///C:/Users/Mitch/Desktop/parry-gripp-do-you-like-waffles.mp3" hidden="true" type="video/quicktime"></embed>
